ccd39f4e872f7f99ce788267b04bf39486949e44
Isaac Adewumi <ienioladewumi@gmail.com>
2024-04-27 19:00:19 +0100

    project setup

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox-viem'
+import '@nomicfoundation/hardhat-chai-matchers'
+import '@nomicfoundation/hardhat-verify'
+import 'hardhat-abi-exporter'
+import 'dotenv/config'
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.24',
+  abiExporter: {
+    path: './abis',
+    pretty: true,
+  },
+  networks: {
+    scrollSepolia: {
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
+      url: 'https://rpc.ankr.com/scroll_sepolia_testnet',
+    },
+    hardhat: {
+      forking: {
+        url: 'https://rpc.ankr.com/scroll_sepolia_testnet',
+        blockNumber: 4095049,
+      },
+    },
+  },
+  etherscan: {
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia-optimism.etherscan.io/',
+        },
+      },
+    ],
+    apiKey: {
+      scrollSepolia: process.env.SCROLL_SEPOLIA_API_KEY!,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+}
+
+export default config
